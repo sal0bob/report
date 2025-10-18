2f883689f58b09528221add186a0d0805de4a58c
sal0bob <urao05092009@gmail.com>
2025-10-18 16:26:25 +0300

    leson1

---
+++ leson1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //коментарий
+        System.out.println("Hello world!");
+
+        /*System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");*/
+    }
+}
