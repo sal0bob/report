ad573dd7b83d42407c1b2d344f22a2370c4193aa
sal0bob <urao05092009@gmail.com>
2025-10-18 16:26:44 +0300

    leson6

---
+++ leson6/app/src/main/java/com/example/leson6/MyView.java
@@ -0,0 +1,24 @@
+package com.example.leson6;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+
+public class MyView extends View {
+    // конструктор – обсуждается в курсе позднее
+    public MyView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint paint = new Paint();
+        int y = 0;
+        while (y < canvas.getHeight()) {
+            canvas.drawLine(0, y,
+                    this.getWidth(), y, paint);
+            y += 30;
+        }
+    }
+}
